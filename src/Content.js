0fa5a34dbb7d15aad2373639e0bb84ecb77ebba7
almcarvalho <antoniolucas@live.com>
2022-11-29 21:22:01 -0300

    alteracoes

--- src/Content.js
+++ src/Content.js
@@ -1,25 +1,35 @@
+import { useState } from "react";
+
 const Content = () => {
+
+    const [name, setName] = useState('?');
 
     const gerenciaNomes = () => {
         const names = ['carne', 'açucar', 'macarrão'];
         var sorteio = Math.random();
         var int = Math.floor(sorteio * 3);
         //console.log(int);
-        return names[int];
+        setName(names[int]);
     }
 
+    //shift alt down
+    //ctrl b
     const handleClick = () => {
         console.log('Houve um click aqui!');
+    }
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`);
     }
 
     return (
         <main>
             <p>
-                Comprar {gerenciaNomes()}!
+                Comprar {name}
             </p>
-            <button onClick={handleClick}>
-                Click it
+            <button onClick={() => gerenciaNomes()}>
+                Sortear
             </button>
+            {/* 44:30 */}
         </main>
     )
 }
