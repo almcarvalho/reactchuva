12b40bbda9e72e66c514c48f409d13d27238f4c7
almcarvalho <antoniolucas@live.com>
2022-11-30 16:04:41 -0300

    arrays e listing

--- src/Content.js
+++ src/Content.js
@@ -2,34 +2,60 @@ import { useState } from "react";
 
 const Content = () => {
 
-    const [name, setName] = useState('?');
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: false,
+            item: "Coca-cola"
+        },
+        {
+            id: 2,
+            checked: true,
+            item: "Fanta"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Guaraná"
+        }
+    ]);
 
-    const gerenciaNomes = () => {
-        const names = ['carne', 'açucar', 'macarrão'];
-        var sorteio = Math.random();
-        var int = Math.floor(sorteio * 3);
-        //console.log(int);
-        setName(names[int]);
+
+    const handleCheck = (id) => {
+        // console.log(`key:  ${id}`);
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+        setItems(listItems);
+        localStorage.setItem('cart', JSON.stringify(listItems));
     }
 
-    //shift alt down
-    //ctrl b
-    const handleClick = () => {
-        console.log('Houve um click aqui!');
-    }
-    const handleClick2 = (name) => {
-        console.log(`${name} was clicked`);
+    const handleDelete = (id) => {
+        //console.log(`del:  ${id}`);
+        const listItems = items.filter((item) => item.id !== id);
+        setItems(listItems);
+        localStorage.setItem('cart', JSON.stringify(listItems));
     }
 
     return (
         <main>
-            <p>
-                Comprar {name}
-            </p>
-            <button onClick={() => gerenciaNomes()}>
-                Sortear
-            </button>
-            {/* 44:30 */}
+            {items.length ? (
+                <ul>
+                    {items.map((item) => (
+                        <li className="item" key={item.id}>
+                            <input type={"checkbox"} checked={item.checked} onChange={() => handleCheck(item.id)} />
+                            <label>
+                                {item.item}
+                            </label>
+                            <button onClick={() => handleDelete(item.id)}>
+                                Remover
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p style={{ marginTop: '2rem' }}>Não há Itens na lista!</p>
+            )}
+
+
         </main>
     )
 }
